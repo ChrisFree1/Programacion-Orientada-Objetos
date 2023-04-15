0f6185d2bbef4d79ad4a46d0d264c2b45f31d275
Chris <chrisfree1122@gmail.com>
2023-04-15 15:11:48 -0500

    Cambio del proyecto

--- src/principiosObjetos.java
+++ src/principiosObjetos.java
@@ -7,9 +7,9 @@ public class principiosObjetos {
          Es un paradigma procedural en la programacion
         */
 
-        
+
         System.out.print("Paradigma en la Programación Orientada a Objetos"); // En println hace salto de línea
-        // Con el print no hace un salto de linea
+        // Con el print no hace un salto de linea --- 
 
 
     }
